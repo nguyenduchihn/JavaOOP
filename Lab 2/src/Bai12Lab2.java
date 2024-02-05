301ca542c2b2d35c9accaf46edd2f263e5a2f353
nguyenduchihn <chinhkul8@gmail.com>
2024-02-05 16:04:58 +0700

    Bai 12 Lab 2

---
+++ Lab 2/src/Bai12Lab2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Bai12Lab2 {
+    public static void main(String[] args) {
+        int hangNgang, hangDoc;
+        Scanner sc = new Scanner(System.in);
+
+        //xac dinh mang 2 chieu
+        System.out.print("nhap so phan tu cua hang ngang mang 2 chieu: ");
+        hangNgang = sc.nextInt();
+        System.out.print("nhap so phan tu cua hang doc mang 2 chieu: ");
+        hangDoc = sc.nextInt();
+
+        //khai bao nhap gia tri cho mang
+        int [][] mangHaiChieu = new int[hangNgang][hangDoc];
+            for(int i = 0; i < mangHaiChieu[i].length; i++){
+                for(int j = 0; j < mangHaiChieu[i].length; j++){
+                    System.out.print("Nhap gia tri cho phan tu [" + (i+1) + "] ["+ (j+1) + "] la: ");
+                    mangHaiChieu[i][j] = sc.nextInt();
+                }
+            }
+
+        //khoi tao bien co dinh va tim kiem gia tri lon nhat
+        int max = mangHaiChieu[0][0];
+        for(int i = 0; i < mangHaiChieu[i].length; i++){
+                for(int j = 0; j < mangHaiChieu[i].length; j++){
+                    if(mangHaiChieu[i][j] >= max){
+                        max = mangHaiChieu[i][j];
+                    }
+                }
+        }
+        System.out.println("Gia tri lon nhat cua mang la: " + max);
+        sc.close();
+    }
+}
