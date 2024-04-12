c1bb9811e505c9e213ef78532f24e0994395c711
nguyenduchihn <chinhkul8@gmail.com>
2024-04-12 11:19:25 +0700

    Slide 80 chuong 3

--- Baithuchanh/src/Slide80chuong3/testNhanVien.java
+++ Baithuchanh/src/Slide80chuong3/testNhanVien.java
@@ -5,7 +5,9 @@ public class testNhanVien {
         NhanVienFullTime sep = new NhanVienFullTime("Nguyen Van Sep");
         sep.setLoaiChucVu(Configs.NHAN_VIEN_SEP);
         NhanVienFullTime linh1 = new NhanVienFullTime("Nguyen Thi Linh", 5);
+        linh1.setLoaiChucVu(Configs.NHAN_VIEN_LINH);
         NhanVienFullTime linh2 = new NhanVienFullTime("Tran Van Linh");
+        linh2.setLoaiChucVu(Configs.NHAN_VIEN_LINH);
         NhanVienPartTime thoivu = new NhanVienPartTime("Luu Van Vu", 300);
         sep.tinhLuong();
         linh1.tinhLuong();
