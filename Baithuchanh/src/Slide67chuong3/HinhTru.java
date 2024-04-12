de35fc6cea57f5f8febb6cc53eba83efa8a52e47
nguyenduchihn <chinhkul8@gmail.com>
2024-04-12 10:10:15 +0700

    Slide 67 chuong 3

---
+++ Baithuchanh/src/Slide67chuong3/HinhTru.java
@@ -0,0 +1,8 @@
+package Slide67chuong3;
+
+public class HinhTru extends HinhTron {
+    public static void xuatThongTin() {
+        System.out.println("Day la hinh tru");
+    }
+    
+}
---
+++ Baithuchanh/src/Slide67chuong3/MainClass.java
@@ -0,0 +1,9 @@
+package Slide67chuong3;
+
+public class MainClass {
+    public static void main(String[] args) {
+        HinhTron.xuatThongTin();
+        HinhTru.xuatThongTin();
+    }
+    
+}
