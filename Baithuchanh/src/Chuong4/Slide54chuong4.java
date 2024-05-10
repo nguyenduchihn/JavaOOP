0a10d2ecfe8c417f96bab11a661f1e4e17f3a2ca
nguyenduchihn <chinhkul8@gmail.com>
2024-05-10 09:17:52 +0700

    Silde 54 chuong 4

---
+++ Baithuchanh/src/Chuong4/Slide54chuong4.java
@@ -0,0 +1,34 @@
+package Chuong4;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Slide54chuong4 {
+    public static void main(String[] args) {
+        String n;
+        HashSet<String> hashSetString = new HashSet<>();
+        Scanner sc = new Scanner(System.in);
+
+        //Them cac phan tu
+        hashSetString.add("Nguyen Van A");
+        hashSetString.add("Tran Van B");
+        hashSetString.add("Le Thi C");
+        hashSetString.add("Dang Van D");
+
+        //Hien thi
+        System.out.println("Danh sach ten trong hashSetString: ");
+        System.out.println(hashSetString);
+        System.out.println("Ten can them: ");
+        n = sc.nextLine();
+        
+        //Them phan tu
+        if (!hashSetString.contains(n)) {
+            hashSetString.add(n);
+            System.out.println("Them thanh cong");
+            System.out.println("Cac phan tu sau khi them trong hashSetString: ");
+            System.out.println(hashSetString);
+        } else {
+            System.out.println("Phan tu " + n + " da ton tai! ");
+        }
+    }
+}
